a1913f452e9feab5a3387cb3a928b9729759557a
valentinvoicu <40645856+valentinvoicu@users.noreply.github.com>
2021-02-14 19:18:32 +0200

    Add files via upload
    A simple python file

---
+++ tema1.py
@@ -0,0 +1,32 @@
+
+'''
+print('Problema 1')
+
+var1 = input('Valoarea 1: ')
+var2 = input('Valoarea 2: ')
+var3 = int(var1)+int(var2)
+
+print('1a')
+print('Suma este: ',var3)
+print('Concatenarea este: ',var1+var2)
+
+var5 = var1+var2
+var4 = input('Multiplicator >=2 : ')
+
+#print('1b')
+print('Multiplicarea este: ',var5*int(var4))
+'''
+
+print('Problema 2')
+
+x= 12
+y = 7
+
+print('Paratul sumei este :', (x**2)+(y**2))
+print('Maximul  este :', max(x,y))
+print('Minimul  este :', min(x,y))
+print('Impartire rotunjita la 2 zecimale :', round(x/y,2))
+print('Valoarea absoluta este :', abs(y-x))
+
+print('Problema 3')
+print('Rezultatul este: ',15 * 2 ** 2 / 7 // 3 + 42 % 5-4)
